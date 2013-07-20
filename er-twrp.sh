58e7d87901c8f63db4ee272c3324ea51510d7817
didhiy <didhiy@gmail.com>
2013-07-21 04:45:29 +0700

    Split voodoo color version

--- er-twrp.sh
+++ er-twrp.sh
@@ -6,7 +6,7 @@ fi
 
 DEFCONFIG_STRING=cyanogenmod_epicmtd_er_defconfig
 PRJROOT=$PWD
-CUSTOMVERSION=nutest-jb42_er-twrp-`date +%m%d`
+CUSTOMVERSION=nitest-jb42_er-twrp-`date +%m%d`
 KERNEL_BUILD_DIR=/media/DATA2/NitestGit/android_kernel_samsung_epicmtd
 BOOTIMG_BUILD_DIR=$PRJROOT/create_boot.img
 ZIP_BUILD_DIR=$PRJROOT/zip-er
